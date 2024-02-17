7e1309aa75771be94e44854e64865deb64bce82b
tesa-klebeband <david@hoheisels-welt.de>
2024-02-17 18:57:06 +0100

    Added new return values

--- src/result.h
+++ src/result.h
@@ -2,18 +2,22 @@
 #define RESULT_H
 
 #define SUCCESS 0
-#define HASH_NOT_FOUND 0
-#define PARSE_ERROR 2
-#define FIELD_ERROR 3
-#define TYPE_ERROR 4
-#define GET_ERROR 5
-#define FILE_ERROR 6
-#define UNSUPPORTED 7
-#define INVALID 8
-#define ACCEPTED 9
-#define REJECTED 10
-#define THREAD_LIMIT_REACHED 11
-#define VALUE_ERROR 12
-#define CONNECTION_CLOSED 13
+#define HASH_NOT_FOUND -1
+#define PARSE_ERROR -2
+#define FIELD_ERROR -3
+#define TYPE_ERROR -4
+#define FILE_ERROR -5
+#define ACCEPTED -6
+#define REJECTED -7
+#define THREAD_LIMIT_REACHED -8
+#define VALUE_ERROR -9
+#define INVALID -10
+#define CONNECT_ERROR -11
+#define SOCKET_READ_ERROR -12
+#define SOCKET_WRITE_ERROR -13
+#define SOCKET_DISCONNECTED -14
+#define GET_ERROR -15
+#define LISTEN_FAILED -16
+#define ACCEPT_FAILED -17
 
 #endif
